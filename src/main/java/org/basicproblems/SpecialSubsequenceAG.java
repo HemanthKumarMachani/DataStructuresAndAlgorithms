5b5d7ec97ac7b6c2eda235abbaa6bd809c0acbf5
HemanthKumarMachani <hemanthmachani15@gmail.com>
2024-12-27 18:54:38 -0600

    Speical Subsequence problem

---
+++ src/main/java/org/basicproblems/SpecialSubsequenceAG.java
@@ -0,0 +1,33 @@
+package org.basicproblems;
+
+public class SpecialSubsequenceAG {
+    public static void main(String[] args) {
+
+
+    }
+
+    /*
+    You have given a string A having Uppercase English letters.
+
+You have to find how many times subsequence "AG" is there in the given string.
+
+NOTE: Return the answer modulo 109 + 7 as the answer can be very large.
+     */
+    public static int solve(String A) {
+        long count = 0;
+        int aCount = 0; // Count of 'A' encountered so far
+        long M = 1000000007;
+        for (int i = 0; i < A.length(); i++) {
+            char currentChar = A.charAt(i);
+
+            if (currentChar == 'A') {
+                aCount++;
+            } else if (currentChar == 'G') {
+                // For every 'G' encountered, add the count of 'A' encountered so far
+                count = (count + aCount) % M;
+            }
+        }
+
+        return (int) count;
+    }
+}
--- src/main/java/org/basicproblems/arrays/RangeSumQuery.java
+++ src/main/java/org/basicproblems/arrays/RangeSumQuery.java
@@ -29,6 +29,7 @@ public class RangeSumQuery {
     //A prefix sum is noting but a cumulative sum of sequence of elements of the input list.
     //Generated using pf[i] = p[i-1] + input[i] which means current element of prefix array
     //is obtained by the sum of previous element in prefix array and current element in input array.
+
     /**
      * Computes the range sum for each query in the given 2D array of queries.
      * The sum of elements from index L to R is efficiently calculated using the prefix sum array:
@@ -36,7 +37,7 @@ public class RangeSumQuery {
      * - Otherwise, result = prefix[R] - prefix[L-1]
      *
      * @param inputList The input list of integers.
-     * @param queries A 2D list where each row represents a query [L, R].
+     * @param queries   A 2D list where each row represents a query [L, R].
      * @return A list of integers representing the sum for each query.
      * Time Complexity: O(N + M), where N is the size of the input list, and M is the number of queries.
      */
@@ -68,6 +69,7 @@ public class RangeSumQuery {
         }
         return resultList;
     }
+
     /**
      * Computes the even-index prefix sum for the input list.
      * For each element at even indices, sums the elements cumulatively.
@@ -78,14 +80,17 @@ public class RangeSumQuery {
     //sum of even element prefix sum
     public static List<Integer> evenPrefixSumOfList(List<Integer> inputList) {
         List<Integer> resultList = new ArrayList<>();
-        if (inputList == null || inputList.isEmpty()) { return resultList; }
+        if (inputList == null || inputList.isEmpty()) {
+            return resultList;
+        }
         resultList.add(inputList.getFirst());
-        for (int i=1; i<inputList.size(); i++) {
-            if (i % 2 == 0) resultList.add(resultList.get(i-1)+ inputList.get(i));
-            else resultList.add(resultList.get(i-1));
+        for (int i = 1; i < inputList.size(); i++) {
+            if (i % 2 == 0) resultList.add(resultList.get(i - 1) + inputList.get(i));
+            else resultList.add(resultList.get(i - 1));
         }
         return resultList;
     }
+
     /**
      * Computes the odd-index prefix sum for the input list.
      * For each element at odd indices, sums the elements cumulatively.
@@ -96,19 +101,22 @@ public class RangeSumQuery {
     //sum of odd elements prefix sum
     public static List<Integer> oddPrefixSumOfList(List<Integer> inputList) {
         List<Integer> resultList = new ArrayList<>();
-        if (inputList == null || inputList.isEmpty()) { return resultList; }
+        if (inputList == null || inputList.isEmpty()) {
+            return resultList;
+        }
         resultList.add(inputList.getFirst());
-        for (int i=1; i<inputList.size(); i++) {
-            if (i % 2 != 0) resultList.add(resultList.get(i-1)+ inputList.get(i));
-            else resultList.add(resultList.get(i-1));
+        for (int i = 1; i < inputList.size(); i++) {
+            if (i % 2 != 0) resultList.add(resultList.get(i - 1) + inputList.get(i));
+            else resultList.add(resultList.get(i - 1));
         }
         return resultList;
     }
+
     /**
      * Finds the count of special indices in the array.
      * A special index is an index such that removing the element at this index
      * makes the sum of even-indexed and odd-indexed elements equal.
-     *
+     * <p>
      * Problem Constraints:
      * 1 <= N <= 10^5
      * -10^5 <= A[i] <= 10^5
@@ -120,22 +128,26 @@ public class RangeSumQuery {
     //special index. Special Index ia an indexing after removing which sum of even-indexed elements is equal
     //to sum of odd-indexed elements // return count of special indexes
     public static int specialIndex(List<Integer> inputList) {
-        if (inputList == null || inputList.isEmpty()) { return -1; }
-        int count =0;
+        if (inputList == null || inputList.isEmpty()) {
+            return -1;
+        }
+        int count = 0;
         int n = inputList.size();
         List<Integer> evenPrefixSum = evenPrefixSumOfList(inputList);
         List<Integer> oddPrefixSum = oddPrefixSumOfList(inputList);
-        for (int i=0; i<inputList.size(); i++) {
+        for (int i = 0; i < inputList.size(); i++) {
             int evenSum = 0;
             int oddSum = 0;
-            if(i==0){
-                evenSum = oddPrefixSum.get(n-1)-oddPrefixSum.get(i);
-                oddSum = evenPrefixSum.get(n-1)-evenPrefixSum.get(i);
-            }else{
-                evenSum = evenPrefixSum.get(i-1)+(oddPrefixSum.get(n-1)-oddPrefixSum.get(i));
-                oddSum = oddPrefixSum.get(i-1)+(evenPrefixSum.get(n-1)-evenPrefixSum.get(i));
+            if (i == 0) {
+                evenSum = oddPrefixSum.get(n - 1) - oddPrefixSum.get(i);
+                oddSum = evenPrefixSum.get(n - 1) - evenPrefixSum.get(i);
+            } else {
+                evenSum = evenPrefixSum.get(i - 1) + (oddPrefixSum.get(n - 1) - oddPrefixSum.get(i));
+                oddSum = oddPrefixSum.get(i - 1) + (evenPrefixSum.get(n - 1) - evenPrefixSum.get(i));
             }
-            if(evenSum == oddSum){ count++; }
+            if (evenSum == oddSum) {
+                count++;
+            }
         }
         return count;
     }
