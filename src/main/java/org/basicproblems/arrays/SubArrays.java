39ff5f0cf05994415856a67b577d8caeb59bc5ed
HemanthKumarMachani <hemanthmachani15@gmail.com>
2024-12-31 16:00:32 -0600

    Returning subarray in range question

--- src/main/java/org/basicproblems/arrays/SubArrays.java
+++ src/main/java/org/basicproblems/arrays/SubArrays.java
@@ -26,6 +26,45 @@ public class SubArrays {
         }
         return input;
     }
+    /**
+     * Problem Description:
+     *
+     * Given an array A of length N, return the subarray from B to C.
+     *
+     * Problem Constraints:
+     * - 1 <= N <= 10^5
+     * - 1 <= A[i] <= 10^9
+     * - 0 <= B <= C < N
+     *
+     * Input Format:
+     * - The first argument is an array of integers A.
+     * - The second and third arguments are integers B and C.
+     *
+     * Output Format:
+     * - Return a subarray of A from index B to C (both inclusive).
+     *
+     * Example Input:
+     * Input 1:
+     *  A = [4, 3, 2, 6]
+     *  B = 1
+     *  C = 3
+     * Input 2:
+     *  A = [4, 2, 2]
+     *  B = 0
+     *  C = 1
+     *
+     * Example Output:
+     * Output 1:
+     *  [3, 2, 6]
+     * Output 2:
+     *  [4, 2]
+     *
+     * Example Explanation:
+     * Explanation 1:
+     *  The subarray of A from index 1 to 3 is [3, 2, 6].
+     * Explanation 2:
+     *  The subarray of A from index 0 to 1 is [4, 2].
+     */
     public static List<Integer> subArrayInRange(List<Integer> input, int start, int end) {
         List<Integer> result = new ArrayList<>();
         if(start == end){
