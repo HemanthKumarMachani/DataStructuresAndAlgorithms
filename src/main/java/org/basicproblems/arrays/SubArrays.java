dd6b2821376967e89650725e5619f96766fede67
HemanthKumarMachani <hemanthmachani15@gmail.com>
2024-12-30 18:10:23 -0600

    SubArrays iterating and printing

---
+++ src/main/java/org/basicproblems/arrays/SubArrays.java
@@ -0,0 +1,23 @@
+package org.basicproblems.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubArrays {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4);
+        printSubArrays(list);
+    }
+
+    public static List<Integer> printSubArrays(List<Integer> input) {
+        for(int i=0; i<input.size(); i++){
+            for(int j=i+1; j<input.size(); j++){
+                for(int k=i; k<j; k++){
+                    System.out.println(input.get(k));
+                }
+            }
+        }
+        return input;
+    }
+}
