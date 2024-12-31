8203fb0d16d88e31af650c5440d9f9c7f8bd95bc
HemanthKumarMachani <hemanthmachani15@gmail.com>
2024-12-31 16:37:25 -0600

    Generating and returning all subarrays

--- src/main/java/org/basicproblems/arrays/SubArrays.java
+++ src/main/java/org/basicproblems/arrays/SubArrays.java
@@ -13,6 +13,8 @@ public class SubArrays {
         System.out.println("Returning sub arrays in range: ");
         List<Integer> subArrayInRange = subArrayInRange(input,9,13);
         System.out.println("printing sub arrays in range: " + subArrayInRange);
+        List<List<Integer>> subArrays = generateAllSubArrays(input);
+        System.out.println("Printing sub arrays: " + subArrays);
     }
 
     public static List<Integer> printSubArrays(List<Integer> input) {
@@ -75,5 +77,61 @@ public class SubArrays {
             result.add(input.get(i));
         }
         return result;
+    }
+/*
+Q. Generate all subarrays
+
+Problem Description
+
+You are given an array A of N integers.
+Return a 2D array consisting of all the subarrays of the array
+
+Note : The order of the subarrays in the resulting 2D array does not matter.
+
+Problem Constraints
+
+1 <= N <= 100
+1 <= A[i] <= 105
+
+Input Format
+
+First argument A is an array of integers.
+
+Output Format
+
+Return a 2D array of integers in any order.
+
+Example Input
+
+Input 1:
+A = [1, 2, 3]
+Input 2:
+A = [5, 2, 1, 4]
+
+Example Output
+
+Output 1:
+[[1], [1, 2], [1, 2, 3], [2], [2, 3], [3]]
+Output 2:
+[[1 ], [1 4 ], [2 ], [2 1 ], [2 1 4 ], [4 ], [5 ], [5 2 ], [5 2 1 ], [5 2 1 4 ] ]
+
+
+Example Explanation
+
+For Input 1:
+All the subarrays of the array are returned. There are a total of 6 subarrays.
+For Input 2:
+All the subarrays of the array are returned. There are a total of 10 subarrays.
+ */
+    public static List<List<Integer>> generateAllSubArrays(List<Integer> input) {
+        List<List<Integer>> subArrays = new ArrayList<>();
+        for (int i = 0; i < input.size(); i++) {
+            List<Integer> subArray = new ArrayList<>();
+            for (int j = i; j < input.size(); j++) {
+                subArray.add(input.get(j));
+            }
+            subArrays.add(subArray);
+        }
+        return subArrays;
     }
 }
