3acd493e30775321a28e267282ef50df86932e76
HemanthKumarMachani <hemanthmachani15@gmail.com>
2025-01-02 21:12:33 -0600

    leadersInAnArray problem question

--- src/main/java/org/basicproblems/arrays/SubArrays.java
+++ src/main/java/org/basicproblems/arrays/SubArrays.java
@@ -18,6 +18,9 @@ public class SubArrays {
         System.out.println("Printing sub arrays: " + subArrays);
         List<Integer> nums = Arrays.asList(3,6,2,9,6,4);
         System.out.println("Closest MinMax: "+ closetMinMaxSubArray(nums));
+        List<Integer> randomInput = Arrays.asList(12,4,5,6,1,17,19,17,15,14,7,6);
+        List<Integer> result = leadersInAnArray(randomInput);
+        System.out.println("Leaders: " + result);
 
     }
 
@@ -160,7 +163,61 @@ All the subarrays of the array are returned. There are a total of 10 subarrays.
         }
         return ans;
     }
-    public static List<Integer> leaderInAnArray(List<Integer> input) {
+    /*
+    Leaders in an array
+
+Problem Description
+
+Given an integer array A containing N distinct integers, you have to find all the leaders in array A. An element is a leader if it is strictly greater than all the elements to its right side.
+
+NOTE: The rightmost element is always a leader.
+
+
+Problem Constraints
+
+1 <= N <= 105
+1 <= A[i] <= 108
+
+
+Input Format
+
+There is a single input argument which a integer array A
+
+
+Output Format
+
+Return an integer array denoting all the leader elements of the array.
+
+
+Example Input
+
+Input 1:
+ A = [16, 17, 4, 3, 5, 2]
+Input 2:
+ A = [5, 4]
+
+
+Example Output
+
+Output 1:
+[17, 2, 5]
+Output 2:
+[5, 4]
+
+
+Example Explanation
+
+Explanation 1:
+ Element 17 is strictly greater than all the elements on the right side to it.
+ Element 2 is strictly greater than all the elements on the right side to it.
+ Element 5 is strictly greater than all the elements on the right side to it.
+ So we will return these three elements i.e [17, 2, 5], we can also return [2, 5, 17] or [5, 2, 17] or any other ordering.
+Explanation 2:
+ Element 5 is strictly greater than all the elements on the right side to it.
+ Element 4 is strictly greater than all the elements on the right side to it.
+ So we will return these two elements i.e [5, 4], we can also any other ordering.
+     */
+    public static List<Integer> leadersInAnArray(List<Integer> input) {
         List<Integer> leaders = new ArrayList<>();
         int n = input.size();
         int maxRight = input.get(n-1);
