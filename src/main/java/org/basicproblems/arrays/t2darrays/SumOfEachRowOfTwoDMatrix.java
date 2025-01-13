413aeead312f7c8d1d735ec24a7bf1d4fde16555
HemanthKumarMachani <hemanthmachani15@gmail.com>
2025-01-13 16:39:59 -0500

    printing principle diagonal

--- src/main/java/org/basicproblems/arrays/t2darrays/SumOfEachRowOfTwoDMatrix.java
+++ src/main/java/org/basicproblems/arrays/t2darrays/SumOfEachRowOfTwoDMatrix.java
@@ -11,6 +11,12 @@ public class SumOfEachRowOfTwoDMatrix {
         matrix.add(Arrays.asList(6,7,8));
         matrix.add(Arrays.asList(9,10,11));
         System.out.println(matrix);
+        int [][] arrMatix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        System.out.println(matrix);
         for(int i=0; i<matrix.size(); i++){
             int sum = 0;
             for(int j=0; j<matrix.get(i).size(); j++){
@@ -20,17 +26,25 @@ public class SumOfEachRowOfTwoDMatrix {
         }
         System.out.println();
         System.out.println("sumOfEachColumnOfTwoDMatrix: "+ sumOfEachColumnOfTwoDMatrix(matrix));
+        printPrincipleDiagonal(arrMatix);
     }
 
     public static List<Integer> sumOfEachColumnOfTwoDMatrix(List<List<Integer>> matrix) {
         List<Integer> result = new ArrayList<>();
-        for(int j=0; j<matrix.get(j).size(); j++){
+        for(int j=0; j<matrix.getFirst().size(); j++){
             int sum =0;
-            for(int i=0; i<matrix.get(i).size(); i++){
-                sum += matrix.get(i).get(j);
+            for(int i=0; i<matrix.get(j).size(); i++){
+                sum += matrix.get(j).get(i);
             }
             result.add(sum);
         }
         return result;
+    }
+
+    public static void printPrincipleDiagonal(int[][] matrix) {
+        System.out.println("Printing principle diagonal: ");
+        for(int i=0; i<matrix.length; i++){
+            System.out.print(matrix[i][i] +" ");
+        }
     }
 }
