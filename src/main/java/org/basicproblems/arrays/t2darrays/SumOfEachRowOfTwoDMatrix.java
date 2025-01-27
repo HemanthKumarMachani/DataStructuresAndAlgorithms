f43c8c9c6cac53f7bec0edd096f0c7739b41de26
HemanthKumarMachani <hemanthmachani15@gmail.com>
2025-01-27 16:05:56 -0500

    printing all diagonals from right to left (bottom to top)

--- src/main/java/org/basicproblems/arrays/t2darrays/SumOfEachRowOfTwoDMatrix.java
+++ src/main/java/org/basicproblems/arrays/t2darrays/SumOfEachRowOfTwoDMatrix.java
@@ -34,6 +34,12 @@ public class SumOfEachRowOfTwoDMatrix {
                 {9, 1, 3, 6, 7},
         };
         printingAllDiagonals(recMatix);
+        List<List<Integer>> recMatrixList = new ArrayList<>();
+        recMatrixList.add(Arrays.asList(1, 3, 7, 8,4));
+        recMatrixList.add(Arrays.asList(2, 0, 1, 8, 2));
+        recMatrixList.add(Arrays.asList(9, 1, 3, 6, 7));
+        System.out.println("recMatrixList: \n"+recMatrixList);
+        printingAllDiagonalsFromRightToLeft(recMatrixList);
     }
 
     public static List<Integer> sumOfEachColumnOfTwoDMatrix(List<List<Integer>> matrix) {
@@ -90,6 +96,35 @@ public class SumOfEachRowOfTwoDMatrix {
             System.out.println();
             if (row > 0) row--;
             else col++;
+        }
+    }
+    // Printing all diagonals from right to left (bottom to top)
+    public static void printingAllDiagonalsFromRightToLeft(List<List<Integer>> matrix) {
+        if (matrix == null || matrix.isEmpty()) {
+            System.out.println("Matrix is empty.");
+            return;
+        }
+        // Check if the matrix is rectangular
+        int n = matrix.size();
+        int m = matrix.getFirst().size();
+        for (List<Integer> rowList : matrix) {
+            if (rowList.size() != m) {
+                throw new IllegalArgumentException("Matrix rows are not of equal length.");
+            }
+        }
+        System.out.println("Printing all diagonals from right to left: ");
+        int currentRow = n - 1, currentCol = m - 1;
+        while (currentCol >= 0) {
+            int diagonalRow = currentRow, diagonalCol = currentCol;
+            System.out.print("Diagonal: ");
+            while (diagonalRow < n && diagonalCol < m && diagonalRow >= 0 && diagonalCol >= 0) {
+                System.out.print(matrix.get(diagonalRow).get(diagonalCol) + " ");
+                diagonalRow++;
+                diagonalCol--;
+            }
+            System.out.println(); // Move to the next diagonal
+            if (currentRow > 0) currentRow--;
+            else currentCol--;
         }
     }
 }
